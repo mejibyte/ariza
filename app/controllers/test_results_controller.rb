572bff1bc244fcd565e630501ffdac8be7c6e0fa
Andrés Mejía-Posada <andmej@gmail.com>
2009-11-08 23:16:31 -0500

    Adding form to create new Test Result

--- app/controllers/test_results_controller.rb
+++ app/controllers/test_results_controller.rb
@@ -1,8 +1,16 @@
 class TestResultsController < ApplicationController
   def new
     @test_result = TestResult.new
+    @questions = Question.all(:include => :answers)
   end
 
   def create
+    @test_result = TestResult.new
+    @test_result.answer_ids = params[:answers].collect { |k, v| v }
+    if @test_result.valid?
+      render :text => "Ok"
+    else
+      render :text => "WTF"
+    end
   end
 end
--- app/models/test_result.rb
+++ app/models/test_result.rb
@@ -1,3 +1,21 @@
 class TestResult < ActiveRecord::Base
   has_and_belongs_to_many :answers
+
+  validate :must_have_exactly_one_answer_for_each_question
+
+
+  protected
+
+  def has_exactly_one_answer_for_each_question?
+    Question.all.each do |question|
+      return false if (answers & question.answers).size != 1
+    end
+    return true
+  end
+
+  def must_have_exactly_one_answer_for_each_question
+    unless has_exactly_one_answer_for_each_question?
+      errors.add(:answers, "must have exactly one answer for each question")
+    end
+  end
 end
