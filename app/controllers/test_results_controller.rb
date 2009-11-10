79972a3fc17e556971c2a28f9af19ca97c16fbb0
Andrés Mejía-Posada <andmej@gmail.com>
2009-11-10 09:35:36 -0500

    Adding Facebooker

--- app/controllers/test_results_controller.rb
+++ app/controllers/test_results_controller.rb
@@ -1,16 +1,21 @@
 class TestResultsController < ApplicationController
+  before_filter :load_questions
   def new
     @test_result = TestResult.new
-    @questions = Question.all(:include => :answers)
   end
 
   def create
     @test_result = TestResult.new
-    @test_result.answer_ids = params[:answers].collect { |k, v| v }
+    @test_result.answer_ids = (params[:answers] || {}).collect { |k, v| v }
     if @test_result.valid?
-      render :text => "Ok"
+      render :text => @test_result.points
     else
-      render :text => "WTF"
+      render :action => "new"
     end
+  end
+
+  protected
+  def load_questions
+    @questions = Question.all(:include => :answers)
   end
 end
