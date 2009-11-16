81c8817dc7b02152c18b9c4defb00f018e2dabc1
Andrés Mejía-Posada <andmej@gmail.com>
2009-11-16 18:54:36 -0500

    Updating js

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def clear
     '<div class="clear"></div>'
   end
+
+  def link_to_group(text)
+    link_to text, "http://www.facebook.com/group.php?gid=125169219846", :target => "_new"
+  end
 end
