3276574c8c459568198ed4544910f43d0744f3e9
Andrés Mejía-Posada <andmej@gmail.com>
2009-11-16 18:59:56 -0500

    Updating js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,10 @@
 var api_key = '47b1e887dce7b235e173024193d326f4';
 var channel_path = '/xd_receiver.html';
 
-FB_RequireFeatures(["Api", "Connect", "XFBML"], function(){
+FB_RequireFeatures(["Api", "Connect", "XFBML", "CanvasUtil"], function(){
     FB.Facebook.init(api_key, channel_path);
     FB.CanvasClient.startTimerToSizeToContent();
+    FB.CanvasClient.scrollTo(0, 0);
 });
 
 $(function(){
