b71de1d0c31a7e6d427b113d21a18e70ddc55d0c
Andrés Mejía-Posada <andmej@gmail.com>
2009-11-17 21:52:55 -0500

    Adding after invite route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :test_results
-  map.resources :invitations
+  map.resources :invitations, :collection => { :after_invite => :post }
   map.root :controller => "test_results", :action => "new"
 end
