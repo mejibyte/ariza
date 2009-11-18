a455e738537c421866ca576e8debc6859910c671
Andrés Mejía-Posada <andmej@gmail.com>
2009-11-17 22:46:40 -0500

    Trying to fix bug with uid

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,13 +2,12 @@
 var api_key = '47b1e887dce7b235e173024193d326f4';
 var channel_path = '/xd_receiver.html';
 
-FB_RequireFeatures(["Api", "Connect", "XFBML", "CanvasUtil"], function(){
-    FB.Facebook.init(api_key, channel_path);
-    FB.CanvasClient.startTimerToSizeToContent();
-    FB.CanvasClient.scrollTo(0, 0);
-    var uid = FB.Facebook.apiClient.get_session().uid;
-    alert(uid);
-    $(function(){
+$(function(){
+    FB_RequireFeatures(["Api", "Connect", "XFBML", "CanvasUtil"], function(){
+        FB.Facebook.init(api_key, channel_path);
+        FB.CanvasClient.startTimerToSizeToContent();
+        FB.CanvasClient.scrollTo(0, 0);
+        var uid = FB.Facebook.apiClient.get_session().uid;
         $("#test_result_uid").val(uid);
     });
 });
