538f98b259815ba42835e8edcff1385ad69baa3e
Andrés Mejía-Posada <andmej@gmail.com>
2009-11-17 21:11:24 -0500

    Adding hack to avoid FB iframe inside FB iframe

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,4 +1,9 @@
 class InvitationsController < ApplicationController
   def new
   end
+
+  def after_invite
+    # Hack to avoid Facebook iframe inside Facebook iframe
+    render :text => "<script language='javascript'>top.location='http://apps.facebook.com/ariza_testi';</script>"
+  end
 end
