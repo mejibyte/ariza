cb3d0ca773417cbadd49285e20d94097f2d1c1ca
Andrés Mejía-Posada <andmej@gmail.com>
2009-11-17 22:32:59 -0500

    Creating uid via Javascript

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,4 @@
+
 var api_key = '47b1e887dce7b235e173024193d326f4';
 var channel_path = '/xd_receiver.html';
 
@@ -5,6 +6,10 @@ FB_RequireFeatures(["Api", "Connect", "XFBML", "CanvasUtil"], function(){
     FB.Facebook.init(api_key, channel_path);
     FB.CanvasClient.startTimerToSizeToContent();
     FB.CanvasClient.scrollTo(0, 0);
+    var uid = FB.Facebook.apiClient.get_session().uid;
+    $(function(){
+        $("#test_result_uid").val(uid);
+    });
 });
 
 $(function(){
